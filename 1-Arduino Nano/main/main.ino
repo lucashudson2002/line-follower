a72d54c9bb49dab78c5be249687b9922638c51b7
Lucas Dias Hudson <lucashudson2002@gmail.com>
2023-02-14 10:40:23 -0300

    adding initial files

---
+++ 1-Arduino Nano/main/main.ino
@@ -0,0 +1,27 @@
+#define SENSOR_MR A0
+#define SENSOR_FR A1
+#define SENSOR_R A2
+#define SENSOR_C A3
+#define SENSOR_L A4
+#define SENSOR_FL A5
+#define SENSOR_ML A6
+#define LED 13
+#define BUZZER 12
+#define PWMA 11
+#define AIN2 10
+#define AIN1 9
+#define STBY 8
+#define BIN1 7
+#define BIN2 6
+#define PWMB 5
+#define BTN 4
+#define ENB1 3
+#define ENA1 2
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
